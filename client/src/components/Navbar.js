29fb15bf4f5d1774ad1ad2c3ed38c9217284ff16
Ryan R <irishreid55@gmail.com>
2023-07-30 10:14:16 -0700

    created a useLogout file

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className="container">
         <Link to="/">
-          <h1>One Day or Day One!</h1>
+          <h1>One Day? or Day One?</h1>
         </Link>
         <nav>
           <div>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
           </div>
         </nav>
       </div>
