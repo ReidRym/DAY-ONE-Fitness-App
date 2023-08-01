790548e589c82cd1044efe39782748fc84665868
Ryan R <irishreid55@gmail.com>
2023-07-31 18:14:26 -0700

    added hook in use Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
+
   const handleSubmit = async e => {
     e.preventDefault();
-    console.log(email, password);
+
+    await login(email, password);
   };
 
   return (
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log In</h3>
-      <label>Email:</label>
+
+      <label>Email address:</label>
       <input
         type="email"
         onChange={e => setEmail(e.target.value)}
@@ -22,7 +28,9 @@ const Login = () => {
         onChange={e => setPassword(e.target.value)}
         value={password}
       />
-      <button type="submit">Log In</button>
+
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
