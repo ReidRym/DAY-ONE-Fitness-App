086e05122a962d76a075d3fdfe37b3050751b1cd
Ryan R <irishreid55@gmail.com>
2023-08-04 12:59:47 -0700

    fixing bugs

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const path = require("path"); // Add this line
 
 // express app
 const app = express();
@@ -19,6 +20,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
+
+// Add these lines to serve your React app in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 // connect to db
 mongoose
